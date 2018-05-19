e681e6828384bd3942a19dd8ddc4d700112b8c37
Eric P. Katz <ericpkatz@gmail.com>
2018-05-19 14:15:18 -0400

    commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/facebook/login', (req, res, next)=> {
 app.get('/facebook/login/callback', (req, res, next)=> {
   //https://ek-fb-testing.herokuapp.com/facebook/login/callback?code=AQBMJFF1qHetzErP85Y4tbM4vrqx8WH3dnbk3oHsSPEgFxsRDHtRiO4HLkow9Se5ISOR9O-usoXdz2rXRpda21xGfGmD0geIA6pa-Yc7IYS-gYB3fz0olgknKcxfjU-8PMcCmaoJ6cAJvOjpH1sAeK156ICWhvTnrP5tyWa1ybkmtWG6u--5SAqULNYTkULwf5ecdE41WGQE9pMuEZdHfv7X6XaPCu4dYQ4YWCP2LNZSBVHs3kmQ9P-0SkC92nF9G_lMveCQnp9WZplHFJCslQDDgnLS7pzhC1VVXr6dtkfhD2uP3cZg2wGur5c7AT0IbvM&state=%7B%22%7Bst%3Dstate123abc%2Cds%3D123456789%7D%22%7D#_=_;
   const code = req.query.code;
-  const url = `https://graph.facebook.com/v3.0/oauth/access_token?client_id=${FACEBOOK_APP_ID}&redirect_uri={redirect-uri}&client_secret=${FACEBOOK_SECRET_ID}&code=${code}`
+  const url = `https://graph.facebook.com/v3.0/oauth/access_token?client_id=${FACEBOOK_APP_ID}&redirect_uri=https://ek-fb-testing.herokuapp.com/facebook/login/callback&client_secret=${FACEBOOK_SECRET_ID}&code=${code}`
   request(url, (err, resp, body)=> {
     res.send(err || body);
   });
