e20de2406ac55a5f63cabfb580aebdb9af7f95b1
Eric P. Katz <ericpkatz@gmail.com>
2018-05-19 13:27:58 -0400

    commit

--- server.js
+++ server.js
@@ -16,5 +16,10 @@ if(!FACEBOOK_APP_ID){
 
 app.get('/', (req, res)=> res.render(path.join(__dirname, 'index.html'), { FACEBOOK_APP_ID }));
 
+app.get('/facebook/login', (req, res, next)=> {
+  const url = `https://www.facebook.com/v3.0/dialog/oauth?client_id={${FACEBOOK_APP_ID}}&redirect_uri={"https://ek-fb-testing.herokuapp.com/facebook/login"}&state={"{st=state123abc,ds=123456789}"}`;
+  res.redirect(url);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`listening on port ${port}`));
