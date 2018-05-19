fd5119e1a743aad82a81c31af4c4e8da47ad532c
Eric P. Katz <ericpkatz@gmail.com>
2018-05-19 13:03:59 -0400

    commit

--- server.js
+++ server.js
@@ -16,4 +16,5 @@ if(!FACEBOOK_APP_ID){
 
 app.get('/', (req, res)=> res.render(path.join(__dirname, 'index.html'), { FACEBOOK_APP_ID }));
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`listening on port ${port}`));
