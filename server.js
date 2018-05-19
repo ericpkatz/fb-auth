ab9cb83f0f5d6ad9ed651a09528ac11e09e0ab39
Eric P. Katz <ericpkatz@gmail.com>
2018-05-19 13:00:50 -0400

    commit

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const { FACEBOOK_APP_ID } = process.env;
 if(!FACEBOOK_APP_ID){
   console.log('set FACEBOOK_APP_ID');
 }
-console.log(FACEBOOK_APP_ID);
 
 app.get('/', (req, res)=> res.render(path.join(__dirname, 'index.html'), { FACEBOOK_APP_ID }));
 
